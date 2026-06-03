43c5c9dfad3c922f7410100810e888e71e01847b
ap9165041-pixel <ap9165041@gmail.com>
2026-06-03 10:51:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 # ===== CONFIG =====
-BOT_TOKEN = "8748370733:AAHmioo1yYD4GcozjnJVVsN8niakHDzmcnE"
+BOT_TOKEN = "8748370733:AAFckHBCRlh5jfS0XfBQuOf4iPDAjWZqri4"
 ADMIN_ID = 8451049817
 
 LIKE_API_KEY = "7d01eb30166546130c171b26eecee191"
